c3efe9526086f7b983bfafe716e24c9b754da220
larwas <1647223050@qq.com>
2020-08-13 15:28:31 +0800

    add github pages

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+//引入 vue.js
 import Vue from 'vue'
+//引入 App.vue
 import App from './App.vue'
-
+//false 阻止Vue启动时生成生产的提示？
 Vue.config.productionTip = false
 
+//创建新的 Vue 实例
 new Vue({
   render: h => h(App),
 }).$mount('#app')
